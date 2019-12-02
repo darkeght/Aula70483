8822c8c69b46f9d2ce3edf4acb7d48c849b2101d
Bruno Guerra <bruno@crescimento.online>
2019-12-02 20:59:40 -0300

    Comitando minhas alterações

---
+++ MinhaAplicacaoConsole/MinhaAplicacaoConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaAplicacaoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá");
+
+            var x = "Felipe";
+
+            Console.WriteLine($"O seu nome é : {x}");
+
+            x = Console.ReadLine();
+
+            Console.WriteLine($"O seu nome é : {x}");
+
+            Console.ReadKey(true);
+
+            
+
+        }
+    }
+}
