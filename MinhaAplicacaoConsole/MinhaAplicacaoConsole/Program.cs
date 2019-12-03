816bf83d18d39d646c79727470d8e810410bd353
Bruno Guerra <bruno@crescimento.online>
2019-12-02 21:51:20 -0300

    Acessando miha lista LINQ com lambida

--- MinhaAplicacaoConsole/MinhaAplicacaoConsole/Program.cs
+++ MinhaAplicacaoConsole/MinhaAplicacaoConsole/Program.cs
@@ -1,28 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MinhaAplicacaoConsole
-{
-    class Program
+{ 
+ class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá");
+            //Aqui criamos o corpo do nosso objeto
+            List<string> 
+                //Damos um nome ao nosso objeto
+                minhaLista 
+                = 
+                //Aqui damos a vida a ela, ou seja iniciamos ele na memoria
+                new List<string>
+            {
+                    //Como resultado podemos adicionar varios nomes ao "minhaLista
+                "Felipe",
+                "Giomar",
+                "Ariosvaldo",
+                "Adagoberto",
+                "Valdisney",
+                "Rusbervaldo"
+            };
 
-            var x = "Felipe";
+            //Chamamos pelo nome minha lista de nome
+            minhaLista
+                .ForEach(//Uma função reservada do linq para ler cada nome da nossa lista
+                x //Aqui temos ja carregado nome por nome
+                => // Falamos que ao lado ele ira passar o nome para utilizar no codigo
+                //Aqui no console mostramos ele em tela para o usuario visualizar
+                Console.WriteLine(x));
 
-            Console.WriteLine($"O seu nome é : {x}");
-
-            x = Console.ReadLine();
-
-            Console.WriteLine($"O seu nome é : {x}");
-
+            //Aqui apenas acionamos um comando para ele parar e assim conseguirmos visualizar as 
+            //informações
             Console.ReadKey(true);
-
-            
 
         }
     }
