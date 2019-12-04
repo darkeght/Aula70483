d10152639605897974ceb2ab3ff6dbabec04ee7d
Bruno Guerra <bruno@crescimento.online>
2019-12-03 22:13:45 -0300

    Commit iniciando orientação

--- RegistroCivilDeNomes/RegistroCivilDeNomes/Program.cs
+++ RegistroCivilDeNomes/RegistroCivilDeNomes/Program.cs
@@ -8,45 +8,30 @@ namespace RegistroCivilDeNomes
 {
     class Program
     {
+        //Aqui criamos uma lista estatica de nomes 
         static List<string> ListaDeNomes = new List<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("***********************");
-            Console.WriteLine("**SISTEMA DE REGISTRO**");
-            Console.WriteLine("***********************");
+            //Aqui invocamos nosso metodo 
+            MensagemInicial();
 
+            //aqui definimos um valor padrão para ele ja iniciar com uma informação
+            //colocamos 0 para ele poder entrar pois ele tem como 5 para sair do sistema
             var menuEscolhido = "0";
-
+            //Aqui temos nosso "while" o while fica perguntado se é diferente de 5 todo momento 
+            //se for diferente de 5 ele continua executando quando for igual a 5 ele sai
             while(menuEscolhido != "5")
             {
-                Console.WriteLine("Digite o número para a opção desejada:");
-                Console.WriteLine("1 - Registrar Nome");
-                Console.WriteLine("2 - Listar Nomes");
-                Console.WriteLine("3 - Editar Um Nome");
-                Console.WriteLine("4 - Excluir Nomes");
-                Console.WriteLine("5 - Sair do sistema");
-
-                menuEscolhido = Console.ReadLine();
-
+                //aqui chamamos primeiro uma variavel para contem o menu escolhido
+                menuEscolhido = MostrarMenu();
+                //
                 switch (menuEscolhido)
                 {
                     case "1":
-                        Console.WriteLine("Registro de Nome Selecionado");
-                        Console.WriteLine("Informa um nome:");
-                        var nomeInformado = Console.ReadLine();
-
-                        ListaDeNomes.Add(nomeInformado);
-
-                        Console.WriteLine("Nome informado com sucesso!");
-                        Console.ReadKey(true);
+                        RegistrarNome();
                         break;
                     case "2":
-                        Console.WriteLine("Listagem de Nome Selecionada");
-
-                        ListaDeNomes.ForEach(x => Console.WriteLine($"Nome: {x}"));
-
-                        Console.WriteLine("Listagem de nomes finalizada.");
-                        Console.ReadKey(true);
+                        ListarNome();
                         break;
                     default:
                         Console.WriteLine("Menu Inválido");
@@ -57,6 +42,45 @@ namespace RegistroCivilDeNomes
             }
 
             Console.ReadKey(true);
+        }
+
+        static void MensagemInicial()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("**SISTEMA DE REGISTRO**");
+            Console.WriteLine("***********************");
+        }
+        static void ListarNome()
+        {
+            Console.WriteLine("Listagem de Nome Selecionada");
+
+            ListaDeNomes.ForEach(x => Console.WriteLine($"Nome: {x}"));
+
+            Console.WriteLine("Listagem de nomes finalizada.");
+            Console.ReadKey(true);
+
+        }
+        static void RegistrarNome()
+        {
+            Console.WriteLine("Registro de Nome Selecionado");
+            Console.WriteLine("Informa um nome:");
+            var nomeInformado = Console.ReadLine();
+
+            ListaDeNomes.Add(nomeInformado);
+
+            Console.WriteLine("Nome informado com sucesso!");
+            Console.ReadKey(true);
+        }
+        static string MostrarMenu()
+        {
+            Console.WriteLine("Digite o número para a opção desejada:");
+            Console.WriteLine("1 - Registrar Nome");
+            Console.WriteLine("2 - Listar Nomes");
+            Console.WriteLine("3 - Editar Um Nome");
+            Console.WriteLine("4 - Excluir Nomes");
+            Console.WriteLine("5 - Sair do sistema");
+
+            return Console.ReadLine();
         }
     }
 }
